20d31658a44cf9d614c76a492e1e13b8b9e1ad05
unknown <dmena@creditu.com>
2022-12-14 21:21:50 -0600

    Estructura básica del proyecto

---
+++ src/components/Boton/Boton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Boton.css';
+
+function Boton() {
+  return (
+    <div className='boton-container'>
+        Boton
+    </div>
+  )
+}
+
+export default Boton
---
+++ src/components/Contador/Contador.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Contador.css';
+
+function Contador() {
+  return (
+    <div className='contador-container'>
+        Contador
+    </div>
+  )
+}
+
+export default Contador
